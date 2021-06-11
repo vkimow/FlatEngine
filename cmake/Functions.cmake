85f97452483441b4179d182e942adb1a4f1196ca
Akimov Egor <akimov.e@panoramik.ru>
2021-06-11 19:54:22 +0300

    Add Config Support

--- cmake/Functions.cmake
+++ cmake/Functions.cmake
@@ -1,15 +1,3 @@
-macro(define_build_type)
-    if(CMAKE_BUILD_TYPE STREQUAL "Debug")
-        set(IS_DEBUG TRUE)
-        set(BUILD_TYPE_DIR "debug")
-    elseif(CMAKE_BUILD_TYPE STREQUAL "Release")
-        set(IS_RELEASE TRUE)
-        set(BUILD_TYPE_DIR "release")
-    else()
-        message(FATAL_ERROR "You are using unsupported build type! ${CMAKE_BUILD_TYPE}")
-    endif()
-endmacro()
-
 macro(define_dirs)
     set(FLAT_ROOT_DIR       "${CMAKE_SOURCE_DIR}")
     set(FLAT_INCLUDE_DIR    "${FLAT_ROOT_DIR}/include")
@@ -17,7 +5,6 @@ macro(define_dirs)
     set(FLAT_VENDOR_DIR     "${FLAT_ROOT_DIR}/vendor")
     set(FLAT_PROJECT_DIR    "${FLAT_ROOT_DIR}/project")
     set(FLAT_BIN_DIR        "${FLAT_ROOT_DIR}/bin")
-    set(FLAT_CONFIG_BIN_DIR "${FLAT_BIN_DIR}/${BUILD_TYPE_DIR}")
 endmacro()
 
 macro(define_global_properties)
@@ -27,9 +14,9 @@ macro(define_global_properties)
     set_property(GLOBAL PROPERTY USE_FOLDERS ON)
     set_property(GLOBAL PROPERTY PREDEFINED_TARGETS_FOLDER "CMake")
     
-    set(CMAKE_RUNTIME_OUTPUT_DIRECTORY "${FLAT_CONFIG_BIN_DIR}/CMake")
-    set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY "${FLAT_CONFIG_BIN_DIR}/CMake")
-    set(CMAKE_LIBRARY_OUTPUT_DIRECTORY "${FLAT_CONFIG_BIN_DIR}/CMake")
+    set(CMAKE_RUNTIME_OUTPUT_DIRECTORY "${FLAT_BIN_DIR}/$<$<CONFIG:Debug>:debug>$<$<CONFIG:Release>:release>/CMake")
+    set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY "${FLAT_BIN_DIR}/$<$<CONFIG:Debug>:debug>$<$<CONFIG:Release>:release>/CMake")
+    set(CMAKE_LIBRARY_OUTPUT_DIRECTORY "${FLAT_BIN_DIR}/$<$<CONFIG:Debug>:debug>$<$<CONFIG:Release>:release>/CMake")
 endmacro()
 
 function(set_flat_main_library)
@@ -52,9 +39,9 @@ function(add_flat_library)
         message(FATAL_ERROR "Extra unparsed arguments when calling add_flat_library: ${THIS_UNPARSED_ARGUMENTS}")
     endif()
 
-
     # create
     add_library(${THIS_LIBRARY_NAME} STATIC ${THIS_SOURCES})
+    include("SetupCONFIGurations")
     target_include_directories(${THIS_LIBRARY_NAME} PRIVATE ${THIS_INCLUDE_DIR})  
 
     # alias
@@ -63,7 +50,8 @@ function(add_flat_library)
 
     # properties
     set_target_properties(${THIS_LIBRARY_NAME} PROPERTIES FOLDER FlatEngine)
-    set_target_properties(${THIS_LIBRARY_NAME} PROPERTIES RUNTIME_OUTPUT_DIRECTORY "${FLAT_CONFIG_BIN_DIR}/${FLAT_PROJECT_NAME}/${THIS_LIBRARY_NAME}")
-    set_target_properties(${THIS_LIBRARY_NAME} PROPERTIES ARCHIVE_OUTPUT_DIRECTORY "${FLAT_CONFIG_BIN_DIR}/${FLAT_PROJECT_NAME}/${THIS_LIBRARY_NAME}")
-    set_target_properties(${THIS_LIBRARY_NAME} PROPERTIES LIBRARY_OUTPUT_DIRECTORY "${FLAT_CONFIG_BIN_DIR}/${FLAT_PROJECT_NAME}/${THIS_LIBRARY_NAME}")
+    set_target_properties(${THIS_LIBRARY_NAME} PROPERTIES RUNTIME_OUTPUT_DIRECTORY "${FLAT_BIN_DIR}/$<$<CONFIG:Debug>:debug>$<$<CONFIG:Release>:release>/${FLAT_PROJECT_NAME}/${THIS_LIBRARY_NAME}")
+    set_target_properties(${THIS_LIBRARY_NAME} PROPERTIES ARCHIVE_OUTPUT_DIRECTORY "${FLAT_BIN_DIR}/$<$<CONFIG:Debug>:debug>$<$<CONFIG:Release>:release>/${FLAT_PROJECT_NAME}/${THIS_LIBRARY_NAME}")
+    set_target_properties(${THIS_LIBRARY_NAME} PROPERTIES LIBRARY_OUTPUT_DIRECTORY "${FLAT_BIN_DIR}/$<$<CONFIG:Debug>:debug>$<$<CONFIG:Release>:release>/${FLAT_PROJECT_NAME}/${THIS_LIBRARY_NAME}")
+    #target_compile_definitions(${THIS_LIBRARY_NAME} $<$<CONFIG:Debug>:debug> $<$<CONFIG:Release>:release>)    
 endfunction()
