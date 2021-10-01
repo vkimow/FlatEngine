dba2f95c8415954cd1ee545743ee2dd50116c1ad
Akimov Egor <akimov.e@panoramik.ru>
2021-10-01 18:58:49 +0300

    Change entry point

--- src/FlatEngine/Sandbox/EntryPoint.cpp
+++ src/FlatEngine/Sandbox/EntryPoint.cpp
@@ -1,8 +1,38 @@
 #include "FlatEngine/Engine/App/Main/App.h"
+#include "FlatEngine/Core/Display/Elements/Shape.h"
+#include "FlatEngine/Engine/Actors/Components/ButtonCompnent.h"
+#include "FlatEngine/Core/Objects/GameObject.h"
+#include "FlatEngine/Core/Main/Events/GlobalFunction.h"
+#include <iostream>
+
+void Overlap()
+{
+	std::cout << "Overlaped!\n";
+}
+
+void Click()
+{
+	std::cout << "Clicked!\n";
+}
 
 int main()
 {
 	Flat::Engine::App app("Sandbox", 480, 480);
+
+	sf::Vector2f size(100, 100);
+	std::shared_ptr<sf::RectangleShape> shape(new sf::RectangleShape(size));
+	std::unique_ptr<Flat::Core::Shape> MyShape(new Flat::Core::Shape(shape));
+
+	std::unique_ptr<Flat::Engine::Actors::ButtonComponent> button(new Flat::Engine::Actors::ButtonComponent(size.x, size.y));
+	Flat::Core::GlobalFunction<void> onOverlap(&Overlap);
+	Flat::Core::GlobalFunction<void> onClick(&Click);
+	button->AddListenerOnButtonOverlaped(onOverlap);
+	button->AddListenerOnButtonClicked(onClick);
+
+	Flat::Core::Objects::GameObject* gameObject = new Flat::Core::Objects::GameObject();
+	gameObject->AddComponent(std::move(MyShape));
+	gameObject->AddComponent(std::move(button));
+
 	app.Start();
 	return 0;
 }
